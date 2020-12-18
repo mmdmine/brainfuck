4579c92e73bcf26d8cc85dd86619604e4e9bd5a9
Mohammad Amin Mollazadeh <the.madamin20@gmail.com>
2020-12-18 21:25:36 +0330

    implement main executable

--- madbf/src/main.c
+++ madbf/src/main.c
@@ -3,20 +3,49 @@
 
 // main.c: Entry point to load MadBf program and execute it
 
+#include <stdio.h>
+
 #include "parser.h"
 #include "vm.h"
 #include "madlib.h"
 
 int main(int argc, char **args) {
-    // TODO: check if argument represents a file or a program
-    // TODO: if argument represents a file then read the file to string
+    if (argc <= 1) {
+        const char *usage_text = "Mad Brainfuck Interpreter\n"
+                                 "By @the_madamin\n\n"
+                                 "Usage: %s <file name>\n"
+                                 "    or %s <program code>\n"
+                                 "    or %s -\n";
 
-    char *program_raw = "+[-[<<[+[--->]-[<<<]]]>>>-]>-.---.>..>.<<<<-.<+.>>>>>.>.<<.<-.";
+        char *argv0 = "madbf";
+
+        if (argc > 0) {
+            argv0 = args[0];
+        }
+
+        printf(usage_text, argv0, argv0, argv0);
+    }
 
     // compile program
     //
     token_vector_t *tokens = token_vector_new(TOKEN_VECTOR_GROW_SIZE);
-    token_vector_stokenize(tokens, program_raw);
+
+    // try to open file
+    FILE *source_file = fopen(args[1], "r");
+
+    if (source_file == NULL) {
+        // try to read argument as program
+        token_vector_stokenize(tokens, args[1]);
+    }
+    else {
+        token_vector_ftokenize(tokens, source_file);
+    }
+
+    if (tokens->count <= 0) {
+        // TODO: error
+        fprintf(stderr, "No program in input");
+    }
+
     // function size could be same as tokens count, or even less
     function_t * main_function = function_new(tokens->count);
     parse_tokens(tokens, main_function);
